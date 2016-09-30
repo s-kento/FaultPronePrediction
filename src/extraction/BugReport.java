ebbddeacc0a6d036141b8734473a191193e4957a
s-kento <s-kento@ist.osaka-u.ac.jp>
2016-09-30 18:30:12 +0900

    csvを読み込んで，BugReport型のリストに格納

---
+++ src/extraction/BugReport.java
@@ -0,0 +1,102 @@
+package extraction;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+//bugreportの属性をフィールド変数にもつ
+public class BugReport {
+	int id;
+	int bug_id;
+	Calendar report_time;
+	int report_timestamp;
+	String status;
+	String commit;
+	int commit_timestamp;
+	String[] file;
+
+	public BugReport(String attr[]) throws ParseException{
+		setId(Integer.parseInt(attr[0]));
+		setBug_id(Integer.parseInt(attr[1]));
+		setReport_time(stringToCalendar(attr[2]));
+		setReport_timestamp(Integer.parseInt(attr[3]));
+		setStatus(attr[4]);
+		setCommit(attr[5]);
+		setCommit_timestamp(Integer.parseInt(attr[6]));
+		setFile(attr[7]);
+	}
+
+	public Calendar stringToCalendar(String str) throws ParseException{
+		final String DATE_PATTERN="yyyy/MM/dd HH:mm";
+		SimpleDateFormat dateformat = new SimpleDateFormat(DATE_PATTERN);
+		Date date = new Date(dateformat.parse(str).getTime());
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		return cal;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getBug_id() {
+		return bug_id;
+	}
+
+	public void setBug_id(int bug_id) {
+		this.bug_id = bug_id;
+	}
+
+	public Calendar getReport_time() {
+		return report_time;
+	}
+
+	public void setReport_time(Calendar report_time) {
+		this.report_time = report_time;
+	}
+
+	public int getReport_timestamp() {
+		return report_timestamp;
+	}
+
+	public void setReport_timestamp(int report_timestamp) {
+		this.report_timestamp = report_timestamp;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getCommit() {
+		return commit;
+	}
+
+	public void setCommit(String commit) {
+		this.commit = commit;
+	}
+
+	public int getCommit_timestamp() {
+		return commit_timestamp;
+	}
+
+	public void setCommit_timestamp(int commit_timestamp) {
+		this.commit_timestamp = commit_timestamp;
+	}
+
+	public String[] getFile() {
+		return file;
+	}
+
+	public void setFile(String file) {
+		this.file = file.split(" ",0);
+	}
+}
---
+++ src/extraction/CSVReader.java
@@ -0,0 +1,24 @@
+package extraction;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVReader {
+	public List<BugReport> readCSV(String file) throws IOException, ParseException{
+		List<BugReport> bugs = new ArrayList<BugReport>();
+		FileReader fr = new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
+		String line=br.readLine();
+		while((line=br.readLine())!=null){
+			String[] attr = line.split(",",0);
+			bugs.add(new BugReport(attr));
+		}
+
+		return bugs;
+	}
+}
