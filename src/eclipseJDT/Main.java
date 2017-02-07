7a034904dd43f6f30ce90f7e5b8bd4b14527e68a
s-kento <s-kento@LAPTOP-M1T8E4IR>
2017-02-07 10:37:24 +0900

    BugInfoをARFF化する機能を実装

--- src/eclipseJDT/Main.java
+++ src/eclipseJDT/Main.java
@@ -3,22 +3,14 @@ package eclipseJDT;
 import java.io.IOException;
 import java.util.List;
 
+import weka.ARFF;
+
 public class Main {
 	public static void main(String[] args) throws IOException {
 		CSVReader reader = new CSVReader();
 		List<BugInfo> bugs = reader.readBugCSV();
 		List<CloneInfo> clones = reader.readCloneCSV();
-		/*int bugNum=0;
-		for (BugInfo bug : bugs) {
-			if (bug.getMetrics()[17] != 0){
-				System.out.println(bug.getClassName());
-				bugNum++;
-			}
-		}
-		System.out.println("バグ含有率は"+(double)bugNum/(double)bugs.size());*/
-		for(CloneInfo clone:clones){
-			System.out.println(clone.getClassName());
-		}
-
+		ARFF arff = new ARFF();
+		arff.makeBugARFF(bugs);
 	}
 }
--- src/weka/ARFF.java
+++ src/weka/ARFF.java
@@ -5,10 +5,32 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+
+import eclipseJDT.BugInfo;
+import eclipseJDT.CloneInfo;
 
 public class ARFF {
-	public void makeARFF() throws IOException{
-		File fileout = new File("output.arff");
+	public void makeBugARFF(List<BugInfo> bugs) throws IOException {
+		PrintWriter pw = makeARFF();
+		for (BugInfo bug : bugs) {
+			for (int value : bug.getMetrics()) {
+				pw.print(value + ",");
+			}
+			if (bug.isFaulty())
+				pw.println("TRUE");
+			else
+				pw.println("FALSE");
+		}
+		pw.close();
+	}
+
+	public void makeCloneARFF(BugInfo bug, CloneInfo clone) {
+
+	}
+
+	public PrintWriter makeARFF() throws IOException {
+		File fileout = new File("result.arff");
 		FileWriter filewriter = new FileWriter(fileout);
 		BufferedWriter bw = new BufferedWriter(filewriter);
 		PrintWriter pw = new PrintWriter(bw);
@@ -20,9 +42,25 @@ public class ARFF {
 		pw.println("@attribute FANIN  numeric");
 		pw.println("@attribute FANOUT   numeric");
 		pw.println("@attribute LCOM numeric");
+		pw.println("@attribute NOC numeric");
+		pw.println("@attribute LCOM numeric");
+		pw.println("@attribute NOA numeric");
+		pw.println("@attribute NOAI numeric");
+		pw.println("@attribute LOC numeric");
+		pw.println("@attribute NOM numeric");
+		pw.println("@attribute NOMI numeric");
+		pw.println("@attribute LCOM numeric");
+		pw.println("@attribute NOPRA numeric");
+		pw.println("@attribute NOPRM numeric");
+		pw.println("@attribute NOPA numeric");
+		pw.println("@attribute NOPM numeric");
+		pw.println("@attribute RFC numeric");
+		pw.println("@attribute WMC numeric\n");
 
-
+		pw.println("@attribute FAULTY {TRUE, FALSE}\n");
 
 		pw.println("@data");
+
+		return pw;
 	}
 }
